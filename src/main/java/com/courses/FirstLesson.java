5e307aac989250005f41949c85debd3d502cfe17
anna turchyna <annalutisha@gmail.com>
2021-03-04 20:06:42 +0200

    Initial commit

---
+++ src/main/java/com/courses/FirstLesson.java
@@ -0,0 +1,33 @@
+package com.courses;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\annal\\IdeaProjects\\qaauto-03.02.2021\\src\\main\\resources\\chromedriver_win32\\chromedriver.exe");
+
+        //логический тип данных
+        boolean boolT = true;
+        boolean boolF = false;
+
+        boolean isVisible = true;
+        boolean isNotVisible = false;
+
+        //byte 8 bits -128 to 127
+        byte bt = 127;
+
+        // short 16 bits -32768 to 32767
+        short shrt = 32767;
+
+        //integer 32 bits -2 147 483 648 to 2 147 483 647
+        int integer = 2147483647;
+
+        //long 64 bits -9,223,372,036,854,775,808 to  9,223,372,036,854,775,807
+        long lng = 9223372036854775807L;
+
+        //flout 32 bits 1.23e100f, -1.23e-100f, .3f, 3.14F
+        flout flt = 1.42453452454575676;
+
+        //double 64 bits 1.79769313486231570 E + 308 to -4.94065645841246544 E-324
+        double dbl = 1.79769313486231570 ;
+
+        //char 16 bits Unicode characters
+        char cr = 'c?/ etc.';
---
+++ src/main/java/com/courses/SecondLesson.java
@@ -0,0 +1,11 @@
+package com.courses;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+  //      System.setProperty("webdriver.chrome.driver", "C:\\Users\\annal\\IdeaProjects\\qaauto-03.02.2021\\src\\main\\resources\\chromedriver_win32\\chromedriver.exe");
+WebDriverManager.chromedriver().setup():
+    WebDriver driver = new ChromeDriver():
+    driver.get ("https://www.google.com"):
